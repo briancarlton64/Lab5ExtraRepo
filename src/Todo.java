8fee992371fc1ab20232166254c00aca8769ef19
Brian Carlton <brian.carlton@ou.edu>
2019-02-19 23:36:40 -0600

    First commit

---
+++ src/Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-19
+OU ID_______________: 113109413
+OU EMAIL____________: brian.carlton@ou.edu
+
+Tasks:
+1. implement calculateTwoTokens
+2. implement calculateThreeTokens
+3. implement execute
+4. implement parseAndExecute
+5. implement CalculatorTest methods
+
+Estimated Time (minutes):
+1. 3
+2. 4
+3. 5
+4. 6
+5.10
+
+Actual Time (minutes):
+1. 3
+2. 5
+3. 6
+4. 8
+5. 13
+*/}
